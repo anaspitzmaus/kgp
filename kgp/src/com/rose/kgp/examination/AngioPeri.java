a8407cc76d385c9b51e6d6fdefb9d744a25c655e
Ekkehard Rose <rose.ekkehard@t-online.de>
2018-12-25 14:55:25 +0100

    class AngioPeri created

---
+++ kgp/src/com/rose/kgp/examination/AngioPeri.java
@@ -0,0 +1,19 @@
+package com.rose.kgp.examination;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AngioPeri extends Catheter_Intervention{
+
+	public AngioPeri(HashMap<String, HashMap<String, ArrayList<String>>> values) {
+		super(values);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setStaff() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
