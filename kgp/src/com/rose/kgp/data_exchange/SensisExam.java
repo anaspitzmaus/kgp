5df779436705a4ae54d17be3a5025db98d140cd7
Ekkehard Rose <rose.ekkehard@t-online.de>
2019-01-04 23:32:17 +0100

    changes

---
+++ kgp/src/com/rose/kgp/data_exchange/SensisExam.java
@@ -0,0 +1,9 @@
+package com.rose.kgp.data_exchange;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public interface SensisExam {
+
+	void addExamData(HashMap<String, HashMap<String, ArrayList<String>>> studyData);
+}
